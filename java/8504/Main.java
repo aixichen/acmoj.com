5dc9826d7c1f87f7e4eac99d94fee50fc2176218
mandarava <927395536@qq.com>
2018-11-09 17:06:46 +0800

    init

--- java/8504/Main.java
+++ java/8504/Main.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+import java.io.File;
+
 public class Main{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        //Scanner cin =new Scanner(new File("data.in"));
         Scanner cin =new Scanner(System.in);
+        StringBuilder bigNumber= new StringBuilder();
+        while(cin.hasNext())
+        {
+            int count=0,sum=0;
+        
+            
+            bigNumber=new StringBuilder(cin.next());
+            if(bigNumber.length()<=1){
+                System.out.println(0);
+            }else{
+                boolean tag=true;
+               
+                while(true){
+                    count++;
+                    for (int i =0 ; i < bigNumber.length(); i++) {
+                        sum+=Integer.parseInt(bigNumber.charAt(i)+"");
+                    }
+                    if(sum<10){
+                        System.out.println(count);
+                        break;
+                    }else{
+                        bigNumber=new StringBuilder(sum+"");
+                        sum=0;
+                    }
+
+                }
+            }
+          
+          
+        }
     }
 }
