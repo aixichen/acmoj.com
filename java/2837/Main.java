47a88bbd25d819a0088b1dc60316a0891b52a37f
mandarava <927395536@qq.com>
2018-11-09 16:26:10 +0800

    init

---
+++ java/2837/Main.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner cin =new Scanner(new File("data.in"));
+        int a,b;
+        while(cin.hasNext()){
+            a=cin.nextInt();
+            b=cin.nextInt();
+            System.out.println(a+b);
+        }  
+    }
+}
